f0ef2e703e7ff2abe547b7f2d9c508ff6bb2fda5
NonPlayChar <87994490+NonPlayChar@users.noreply.github.com>
2026-04-06 01:11:48 +0300

    Update functions
    Update sql query from * format and reordered functions

--- database.py
+++ database.py
@@ -16,6 +16,9 @@ def initiate_database() -> None:
     conn.close()
     print('database: Ready!')
 
+
+
+
 def create_user(uname, pswrd, pfp=None) -> None:
     username, password, uid = uname, hashit(pswrd), token(fetch_userids())
     
@@ -30,39 +33,36 @@ def create_user(uname, pswrd, pfp=None) -> None:
     db.commit()
     db.close()
 
-def fetch_userids() -> list:
-    db = sqlite3.connect('database.db')
-    userids = db.execute('''SELECT userid FROM userdata''').fetchall()
-    return userids
 
-def fetch_postids() -> list:
+def login_user(uname, pswrd) -> any:
     db = sqlite3.connect('database.db')
-    postids = db.execute('''SELECT postid FROM posts''').fetchall()
-    return postids
+    epwrd = hashit(pswrd)
+    result = db.execute('''SELECT userid, username, password FROM userdata
+               WHERE username = ?
+               ''', (uname,)).fetchall()
+    if len(result) == 0:
+        return ValueError('Invalid username or password!')
+    result = result[0]
+    if epwrd == result[2]:
+        return result[0:2]
+    return ValueError('Invalid username or password!')
+
 
 def fetch_user(uid):
     db = sqlite3.connect('database.db')
     cursor = db.cursor()
-    fetch_result = cursor.execute('''SELECT * FROM userdata
+    fetch_result = cursor.execute('''SELECT userid, username, password, pfp, joindate FROM userdata
                WHERE userid = ?
                ''', (uid,)).fetchall()[0]
     column_names = [description[0] for description in cursor.description]
     return to_dict(column_names, fetch_result)
 
-def login_user(uname, pswrd) -> any:
+
+def fetch_userids() -> list:
     db = sqlite3.connect('database.db')
-    epwrd = hashit(pswrd)
-    result = db.execute('''SELECT * FROM userdata
-               WHERE username = ?
-               ''', (uname,)).fetchall()
+    userids = db.execute('''SELECT userid FROM userdata''').fetchall()
+    return userids
 
-    if len(result) == 0:
-        return ValueError('Invalid username or password!')
-    result = result[0]
-
-    if epwrd == result[2]:
-        return result[0:2]
-    return ValueError('Invalid username or password!')
 
 def create_post(uid: int, t, d):
     db = sqlite3.connect('database.db')
@@ -71,55 +71,6 @@ def create_post(uid: int, t, d):
                 ''', (token(fetch_postids()), int(uid), t, d))
     db.commit()
     db.close()
-
-def fetch_posts():
-    db = sqlite3.connect('database.db')
-    cursor = db.cursor()
-    scan = cursor.execute("SELECT * FROM posts ORDER BY timestamp DESC")
-
-    column_names = [description[0] for description in cursor.description]
-    posts = list()
-
-    for post in scan:
-        
-        posts.append(to_dict(column_names, post))
-
-    return posts
-
-def fetch_userposts(userid: int) -> list:
-    db = sqlite3.connect('database.db')
-    cursor = db.cursor()
-    userposts = cursor.execute('SELECT * FROM posts WHERE userid = ? ORDER BY timestamp DESC', (userid, ))
-
-    column_names = [description[0] for description in cursor.description]
-    posts = list()
-
-    for post in userposts:
-        posts.append(to_dict(column_names, post))
-    return posts
-
-def fetch_post(postid: int):
-    db = sqlite3.connect('database.db')
-    cursor = db.cursor()
-    fetch_result = cursor.execute('SELECT * FROM posts WHERE postid = ?', (postid, )).fetchall()[0]
-    column_names = [description[0] for description in cursor.description]
-    return to_dict(column_names, fetch_result)
-
-
-def search_post(query):
-    db = sqlite3.connect('database.db')
-    cursor = db.cursor()
-    fetch_result = cursor.execute('''
-            SELECT *
-            FROM posts p
-            JOIN userdata u ON u.userid = p.userid        
-            WHERE p.content LIKE ? OR p.title LIKE ?
-            ORDER BY p.timestamp DESC''', ("%" + query + "%", "%" + query + "%")).fetchall()
-    posts = list()
-    column_names = [description[0] for description in cursor.description]
-    for post in fetch_result:
-        posts.append(to_dict(column_names, post))
-    return posts
 
 
 def update_content(content, postid):
@@ -143,3 +94,66 @@ def delete_post(postid):
         print('Error:', e)
         db.close()
         return False
+
+
+def fetch_post(postid: int):
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    fetch_result = cursor.execute('SELECT postid, userid, title, content, timestamp FROM posts WHERE postid = ?', (postid, )).fetchall()[0]
+    column_names = [description[0] for description in cursor.description]
+    return to_dict(column_names, fetch_result)
+
+
+def fetch_postids() -> list:
+    db = sqlite3.connect('database.db')
+    postids = db.execute('''SELECT postid FROM posts''').fetchall()
+    return postids
+
+
+def fetch_userposts(userid: int) -> list:
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    userposts = cursor.execute('SELECT postid, userid, title, content, timestamp FROM posts WHERE userid = ? ORDER BY timestamp DESC', (userid, ))
+
+    column_names = [description[0] for description in cursor.description]
+    posts = list()
+
+    for post in userposts:
+        posts.append(to_dict(column_names, post))
+    return posts
+
+
+def fetch_posts():
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    scan = cursor.execute("SELECT postid, userid, title, content, timestamp FROM posts ORDER BY timestamp DESC")
+
+    column_names = [description[0] for description in cursor.description]
+    posts = list()
+
+    for post in scan:
+        
+        posts.append(to_dict(column_names, post))
+
+    return posts
+
+
+def search_post(query):
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    fetch_result = cursor.execute('''
+            SELECT postid, userid, title, content, timestamp
+            FROM posts p
+            JOIN userdata u ON u.userid = p.userid        
+            WHERE p.content LIKE ? OR p.title LIKE ?
+            ORDER BY p.timestamp DESC''', ("%" + query + "%", "%" + query + "%")).fetchall()
+    posts = list()
+    column_names = [description[0] for description in cursor.description]
+    for post in fetch_result:
+        posts.append(to_dict(column_names, post))
+    return posts
+
+
+
+
+
